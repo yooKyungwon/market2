7d60aec4d0b134f01a0f3c8552ebcc2ba860bc4c
yoo <yooguwon@gmail.com>
2015-06-04 04:14:24 +0900

    anding sql

---
+++ market1.sql
@@ -0,0 +1,96 @@
+/*
+SQLyog Community v12.09 (32 bit)
+MySQL - 5.6.24-log : Database - market
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`market` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `market`;
+
+/*Table structure for table `bag` */
+
+DROP TABLE IF EXISTS `bag`;
+
+CREATE TABLE `bag` (
+  `index` int(11) NOT NULL AUTO_INCREMENT,
+  `cart` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  PRIMARY KEY (`index`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+/*Data for the table `bag` */
+
+/*Table structure for table `barcode` */
+
+DROP TABLE IF EXISTS `barcode`;
+
+CREATE TABLE `barcode` (
+  `index` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `barcode` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`index`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `barcode` */
+
+insert  into `barcode`(`index`,`name`,`price`,`barcode`) values (1,'snack',1500,'y12h3kd3'),(2,'juice',1200,'j3akc45'),(3,'plate',2500,'dkk13kl1');
+
+/*Table structure for table `cart` */
+
+DROP TABLE IF EXISTS `cart`;
+
+CREATE TABLE `cart` (
+  `index` int(11) NOT NULL AUTO_INCREMENT,
+  `number` varchar(255) DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  PRIMARY KEY (`index`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Data for the table `cart` */
+
+insert  into `cart`(`index`,`number`,`state`) values (1,'1',0),(2,'2',0),(3,'3',0),(4,'4',0),(5,'5',0);
+
+/*Table structure for table `cartstate` */
+
+DROP TABLE IF EXISTS `cartstate`;
+
+CREATE TABLE `cartstate` (
+  `index` int(11) NOT NULL AUTO_INCREMENT,
+  `cart` varchar(255) DEFAULT NULL,
+  `userid` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`index`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `cartstate` */
+
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `index` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`index`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `login` */
+
+insert  into `login`(`index`,`userid`,`password`) values (1,'',''),(2,'huijun123@nate.com','gmlwns123');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
